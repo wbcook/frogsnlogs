b708870393283cd23c4b8cca3033202c1b2b2aa2
William Cook <wbcook@users.noreply.github.com>
2015-09-05 17:31:54 -0400

    Create PickupParticlesDestruction.cs

---
+++ PickupParticlesDestruction.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using System.Collections;
+
+public class PickupParticlesDestruction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		// Destroy the pickup particles after 5 seconds
+		Destroy (gameObject, 5f); // this game object
+	
+	}
+
+}
