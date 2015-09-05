104027c8b0336c16094e107083fe1111396e3c1c
William Cook <wbcook@users.noreply.github.com>
2015-09-05 17:55:56 -0400

    Create FlySpawner.cs

---
+++ FlySpawner.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using System.Collections;
+
+public class FlySpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject flyPrefab;
+	[SerializeField]
+	private int totalFlyMinimum = 12;
+
+	private float spawnArea = 25f;
+
+	public static int totalFlies;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
