626aaf60270e01ded5a4c7bc58d92b3165c99205
William Cook <wbcook@users.noreply.github.com>
2015-09-05 23:01:26 -0400

    Update FlySpawner.cs

--- FlySpawner.cs
+++ FlySpawner.cs
@@ -14,11 +14,30 @@ public class FlySpawner : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		totalFlies = 0;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		// While the total number of flies is less than the minimum...
+		while (totalFlies < totalFlyMinimum) {
+
+			// ...then increment the total number of flies...
+			totalFlies++;
+
+			// ...create a random position for a fly...
+			float positionX = Random.Range (-spawnArea, spawnArea); // somewhere between the positive and negative edge of the game world
+			float positionZ = Random.Range (-spawnArea, spawnArea);
+
+			Vector3 flyPosition = new Vector3(positionX, 2f, positionZ); // place the flies in the limits and 2 meters high
+
+			// ...and create a new fly
+			Instantiate(flyPrefab, flyPosition, Quaternion.identity);
+
+		}
 	
 	}
 }
